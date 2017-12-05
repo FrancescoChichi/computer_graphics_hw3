e977e51a9c3f74710315871d0949ff40b1a8fbfb
francesco.chichi <francesco.chichi@gmail.com>
2017-12-05 02:38:07 +0100

    first

---
+++ build/CMakeFiles/pathtrace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pathtrace.dir/src/pathtrace.cpp.o"
+  "../bin/pathtrace.pdb"
+  "../bin/pathtrace"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pathtrace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

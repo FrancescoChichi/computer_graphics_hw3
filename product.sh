b38427629481552d1c49c3b104a7dcbbbfd5c049
francesco.chihi <francesco.chichi@gmail.com>
2017-12-09 15:54:37 +0100

    funge

---
+++ product.sh
@@ -0,0 +1,1 @@
+./bin/pathtrace -r 360 -s 256 -i product -p -o out/cornell_box_product.png in/cornell_box/cornell_box.obj
--- src/pathtrace.cpp
+++ src/pathtrace.cpp
@@ -5,6 +5,7 @@
 #include "printData.h"
 
 using namespace ygl;
+
 using rng_t = ygl::rng_pcg32;
 
 constexpr const auto ray_eps = 1e-4f;
@@ -13,12 +14,16 @@ struct point {
   const instance* ist = nullptr;
   vec3f x = zero3f; //position on shp
   vec3f n = zero3f; //normal on shape
-  vec3f le = zero3f;
+  vec3f le = zero3f;//light emission
   vec3f o = zero3f; //outgoing direction
-  vec3f kd = zero3f;
-  vec3f ks = zero3f;
-  float rs = 0.5;
+  vec3f kd = zero3f;//diffuse
+  vec3f ks = zero3f;//specular
+  float rs = 0.5;   //specular roughness
 
+  vec3f kr(float cosw) const {
+    return  ks + (vec3f{1, 1, 1} - ks) * pow(clamp(1.0f - cosw, 0.0f, 1.0f), 5.0f); }
+
+  vec3f kt(float cosw) const{ return vec3f{1, 1, 1}-kr(cosw);}
   bool hit() const { return (bool)ist; }
   bool emission_only() const {
     return kd == vec3f{0, 0, 0} && ks == vec3f{0, 0, 0};
@@ -62,102 +67,6 @@ ray3f sample_camera(const camera* cam, int i, int j, int res, rng_t& rng) {
 point eval_point(const instance* ist, int eid, const vec4f& euv, const vec3f& o) {
 
   auto p = point();
-/*
-  // instance
-  p.ist = ist;
-
-  // outgoing direction
-  p.o = o;
-
-  // shortcuts
-  auto shp = ist->shp;
-  auto mat = ist->shp->mat;
-
-  // compute points and weights
-  auto pos = eval_pos(ist->shp, eid, euv);
-  auto norm = eval_norm(ist->shp, eid, euv);
-  auto texcoord = eval_texcoord(ist->shp, eid, euv);
-  auto color = eval_color(ist->shp, eid, euv);
-
-
-  p.x = pos;
-  p.n = norm;
-
-
-  // handle normal map
-  if (mat->norm_txt) {
-    auto tangsp = eval_tangsp(ist->shp, eid, euv);
-    auto txt = eval_texture(mat->norm_txt, texcoord, false).xyz() * 2.0f -
-               vec3f{1, 1, 1};
-    auto ntxt = normalize(vec3f{txt.x, -txt.y, txt.z});
-    auto frame =
-        make_frame3_fromzx({0, 0, 0}, norm, {tangsp.x, tangsp.y, tangsp.z});
-    frame.y *= tangsp.w;
-    norm = transform_direction(frame, ntxt);
-  }
-
-  // correct for double sided
-  if (mat->double_sided && dot(norm, o) < 0) norm = -norm;
-
-
-  // handle color
-  auto kx_scale = vec4f{1, 1, 1, 1};
-  if (!shp->color.empty()) kx_scale *= color;
-
-  // handle occlusion
-  if (mat->occ_txt)
-    kx_scale.xyz() *= eval_texture(mat->occ_txt, texcoord).xyz();
-
-  // sample emission
-  auto ke = mat->ke * kx_scale.xyz();
-
-  // sample reflectance
-  auto kd = zero4f, ks = zero4f, kt = zero4f;
-  switch (mat->mtype) {
-    case material_type::specular_roughness: {
-      kd = vec4f{mat->kd, mat->op} * kx_scale *
-           eval_texture(mat->kd_txt, texcoord);
-      ks = vec4f{mat->ks, mat->rs} * vec4f{kx_scale.xyz(), 1} *
-           eval_texture(mat->ks_txt, texcoord);
-    }
-      break;
-    case material_type::metallic_roughness: {
-      auto kb = vec4f{mat->kd, mat->op} * kx_scale *
-                eval_texture(mat->kd_txt, texcoord);
-      auto km = vec2f{mat->ks.x, mat->rs};
-      if (mat->ks_txt) {
-        auto ks_txt = eval_texture(mat->ks_txt, texcoord);
-        km.x *= ks_txt.y;
-        km.y *= ks_txt.z;
-      }
-      kd = vec4f{kb.xyz() * (1 - km.x), kb.w};
-      ks =
-          vec4f{kb.xyz() * km.x + vec3f{0.04f, 0.04f, 0.04f} * (1 - km.x),
-                km.y};
-    }
-      break;
-    case material_type::specular_glossiness: {
-      kd = vec4f{mat->kd, mat->op} * kx_scale *
-           eval_texture(mat->kd_txt, texcoord);
-      ks = vec4f{mat->ks, mat->rs} * vec4f{kx_scale.xyz(), 1} *
-           eval_texture(mat->ks_txt, texcoord);
-      ks.w = 1 - ks.w;  // glossiness -> roughness
-    }
-      break;
-  }
-
-  // set up final values
-  p.le = ke * kd.w;
-  p.kd = kd.xyz() * kd.w;
-  p.ks =
-      (ks.xyz() != zero3f && ks.w < 0.9999f) ? ks.xyz() * kd.w : zero3f;
-  p.rs = (ks.xyz() != zero3f && ks.w < 0.9999f) ? ks.w * ks.w : 0;
-
-
-
-  // done
-  return p;*/
-
 
   p.ist=ist;
   p.o = o;
@@ -178,7 +87,8 @@ point eval_point(const environment* env, const vec3f& o) {
 
   //p.le=env->ke;
   p.o= o;
-/*
+  p.le=env->ke;
+  /*
   // maerial
   auto ke = env->ke;
   if (env->ke_txt) {
@@ -188,11 +98,11 @@ point eval_point(const environment* env, const vec3f& o) {
     auto texcoord = vec2f{phi, theta};
     ke *= eval_texture(env->ke_txt, texcoord).xyz();
   }
-*/
-  // create emission lobe
-  //if (ke != zero3f) { p.le =  ke; }
-  if (env->ke != zero3f) { p.le =  env->ke; }
 
+  // create emission lobe
+  if (ke != zero3f) { p.le =  ke; }
+  //if (env->ke != zero3f) { p.le =  env->ke; }
+*/
   // done
   return p;
 
@@ -256,11 +166,44 @@ void init_lights(scene* scn) {
 /// If no lights are present, just return {}.
 /// To use with MIS, fold the cosine at the light and r^2 into this funciton.
 point sample_lights(const scene* scn, const point& pt, rng_t& rng) {
-  if(scn->lights.empty())
+
+  /*if(scn->lights.empty())
     return {};
   else{
 
     //scn->lights[0]->ist->shp->mat->ke;
+    return {};
+  }*/
+
+  auto lgt = scn->lights[0];
+  auto rne = next_rand1f(rng);
+  vec2f rn = {next_rand1f(rng),next_rand1f(rng)};
+
+  if (lgt->ist) {
+    auto shp = lgt->ist->shp;
+    auto eid = 0;
+    auto euv = zero4f;
+    if (!shp->triangles.empty()) {
+      std::tie(eid, (vec3f&)euv) =
+          sample_triangles(shp->elem_cdf, rne, rn);
+    } else if (!shp->points.empty()) {
+      eid = sample_points(shp->elem_cdf, rne);
+      euv = {1, 0, 0, 0};
+    } else {
+      assert(false);
+    }
+    auto lpt = eval_point(lgt->ist, eid, euv, zero3f);
+    lpt.o = normalize(pt.x - lpt.x);
+    return lpt;
+  } else if (lgt->env) {
+    auto z = -1 + 2 * rn.y;
+    auto rr = sqrt(clamp(1 - z * z, (float)0, (float)1));
+    auto phi = 2 * pif * rn.x;
+    auto wo = vec3f{cos(phi) * rr, z, sin(phi) * rr};
+    auto lpt = eval_point(lgt->env, wo);
+    return lpt;
+  } else {
+    assert(false);
     return {};
   }
 }
@@ -268,7 +211,17 @@ point sample_lights(const scene* scn, const point& pt, rng_t& rng) {
 /// Compute the light sampling weight, which 1/pdf
 float weight_lights(const scene* scn, const point& lpt, const point& pt) {
   //TODO weight_lights
-  return {};
+
+
+  if (!lpt.hit()) return 0;
+  // support only one lobe for now
+  if(lpt.emission_only())//env
+    return 4 * pif;
+  else{
+    auto d = dist(lpt.x, pt.x);
+    return lpt.ist->shp->elem_cdf.back() / (d * d);
+  }
+
 }
 
 
@@ -280,12 +233,14 @@ inline float eval_ggx(float rs, float ndh, float ndi, float ndo) {
   auto di = (ndh * ndh) * (alpha2 - 1) + 1;
   auto d = alpha2 / (pif * di * di);
 
-  auto go = (2 * ndo) / (ndo + sqrt(alpha2 + (1 - alpha2) * ndo * ndo));
-  auto gi = (2 * ndi) / (ndi + sqrt(alpha2 + (1 - alpha2) * ndi * ndi));
-  auto g = go * gi;
+  auto lambda_o = (-1 + sqrt(1 + alpha2 * (1 - ndo * ndo) / (ndo * ndo))) / 2;
+  auto lambda_i = (-1 + sqrt(1 + alpha2 * (1 - ndi * ndi) / (ndi * ndi))) / 2;
+  auto g = 1 / (1 + lambda_o + lambda_i);
+
 
   return d * g;
 }
+
 
 
 // Evaluates the GGX pdf
@@ -307,9 +262,10 @@ inline vec3f sample_ggx(float rs, const vec2f& rn) {
   return wh_local;
 }
 
+
 /// Evaluate the BSDF*cosine for a triangle surface. As BSDF use Kd/pi +
 /// ks*D()*G()/4cos()cos(), using the GGX for D
-vec3f eval_triangle_brdfcos(const point& pt, const vec3f& i) {
+vec3f eval_triangle_brdfcos(const point& pt, const vec3f& i) {///OK
   auto brdfcos = zero3f;
 
   const auto& o = pt.o;
@@ -318,10 +274,23 @@ vec3f eval_triangle_brdfcos(const point& pt, const vec3f& i) {
 
   // compute dot products
   auto ndo = dot(n, o), ndi = dot(n, i),
-      ndh = dot(h, n);
+      ndh = clamp(dot(h, n), (float)-1, (float)1);
+  auto odh = clamp(dot(o, h), 0.0f, 1.0f);
 
-  brdfcos = (pt.kd/pif)+
-      ((pt.ks* eval_ggx(pt.rs, ndh, ndi, ndo))/(4*abs(ndi)*abs(ndo)));
+  // diffuse term
+  if (ndi > 0 && ndo > 0) { brdfcos += pt.kd * ndi / pif; }
+
+  // specular term
+  if (ndi > 0 && ndo > 0 && ndh > 0) {
+    // microfacet term
+    auto dg = eval_ggx(pt.rs, ndh, ndi, ndo);
+
+    // sum up
+    brdfcos += pt.ks * ndi * dg / (4 * ndi * ndo);
+  }
+
+  // transmission hack
+  if (o == -i) brdfcos += pt.kt(odh);
 
   return brdfcos;
 }
@@ -377,6 +346,17 @@ vec3f eval_brdfcos(const point& pt, const vec3f& i) {
   }
 }
 
+
+vec3f sample_spherical_dir(const point& pt, rng_t& rng) {
+  auto rn = vec2f{next_rand1f(rng), next_rand1f(rng)};
+  auto rz = rn.y, rr = sqrtf(1 - rz * rz), rphi = 2 * pif * rn.x;
+  auto wi_local = vec3f{rr * cosf(rphi), rr * sinf(rphi), rz};
+  return transform_direction(make_frame3_fromz(pt.x, pt.n), wi_local);
+}
+
+float weight_spherical_dir() { return 1 / (4 * pif); }
+
+
 /// Evaluate the BSDF*cosine as discussed in the slides
 vec3f sample_triangle_brdfcos(const point& pt, rng_t& rng) {
   //TODO sample_triangle_brdfcos
@@ -389,10 +369,12 @@ vec3f sample_triangle_brdfcos(const point& pt, rng_t& rng) {
   if (!pt.hit()) return zero3f;
 
   // probability of each lobe
-  auto kdw = max_element_val(pt.kd), ksw = max_element_val(pt.ks);
-  auto kaw = kdw + ksw;
+  auto kdw = max_element_val(pt.kd), ksw = max_element_val(pt.ks),
+      ktw = max_element_val(pt.kt(dot(wn,-wo)));
+  auto kaw = kdw + ksw;// + ktw;
   kdw /= kaw;
   ksw /= kaw;
+  ktw /= kaw + ktw;
 
   auto rnl = next_rand1f(rng);
   vec2f rn = {next_rand1f(rng),next_rand1f(rng)};
@@ -420,10 +402,11 @@ vec3f sample_triangle_brdfcos(const point& pt, rng_t& rng) {
     // compute wi
     return normalize(wh * 2.0f * dot(wo, wh) - wo);
   }
- /*   // transmission hack
-  else if (rnl < kdw + ksw)
+    // transmission hack
+  else if (rnl < kdw + ksw + ktw) {
     // continue ray direction
-    return -wo;*/
+    //return -wo;
+  }
 
   return zero3f;
 }
@@ -432,15 +415,14 @@ vec3f sample_triangle_brdfcos(const point& pt, rng_t& rng) {
 float weight_triangle_brdfcos(const point& pt, const vec3f& i) {
   //TODO weight_triangle_brdfcos
 
-  // grab variables
-  auto& wn = pt.n;
-  auto& wo = pt.o;
-
   // skip if no component
   if (!pt.hit()) return 0;
 
+
+
   // probability of each lobe
   auto kdw = max_element_val(pt.kd), ksw = max_element_val(pt.ks);
+
   auto kaw = kdw + ksw;
   kdw /= kaw;
   ksw /= kaw;
@@ -449,10 +431,18 @@ float weight_triangle_brdfcos(const point& pt, const vec3f& i) {
   auto pdf = 0.0f;
 
 
-  auto wh = normalize(i + wo);
+  auto h = normalize(i + pt.o);
 
   // compute dot products
-  auto ndo = dot(wn, wo), ndi = dot(wn, i), ndh = dot(wn, wh);
+  auto ndo = dot(pt.n, pt.o),
+      ndi = dot(pt.n, i),
+      ndh = dot(pt.n, h);
+
+  ///p(i)=n.i/pi
+
+// compute wh
+
+  // compute dot products
 
   // diffuse term (hemipherical cosine probability)
   if (ndo > 0 && ndi > 0) { pdf += kdw * ndi / pif; }
@@ -461,20 +451,15 @@ float weight_triangle_brdfcos(const point& pt, const vec3f& i) {
   if (ndo > 0 && ndi > 0 && ndh > 0) {
     // probability proportional to d adjusted by wh projection
     auto d = pdf_ggx(pt.rs, ndh);
-    auto hdo = dot(wo, wh);
+    auto hdo = dot(pt.o, h);
     pdf += ksw * d / (4 * hdo);
   }
+
+  // transmission hack
+  //if (i == -pt.o) pdf += ktw;
+
   return 1 / pdf;
 }
-
-vec3f sample_spherical_dir(const point& pt, rng_t& rng) {
-  auto rn = vec2f{next_rand1f(rng), next_rand1f(rng)};
-  auto rz = rn.y, rr = sqrtf(1 - rz * rz), rphi = 2 * pif * rn.x;
-  auto wi_local = vec3f{rr * cosf(rphi), rr * sinf(rphi), rz};
-  return transform_direction(make_frame3_fromz(pt.x, pt.n), wi_local);
-}
-
-float weight_spherical_dir() { return 1 / (4 * pif); }
 
 /// Sample the BSDF*cosine
 vec3f sample_brdfcos(const point& pt, rng_t& rng) {
@@ -508,7 +493,6 @@ float weight_brdfcos(const point& pt, const vec3f& i) {
 /// In this method, use hemispherical cosine sampling and only lambert BSDF.
 vec3f estimate_li_naive(
     const scene* scn, const vec3f& q, const vec3f& d, int bounces, rng_t& rng) {
-//          auto li = estimate_li(scn, ray.o, ray.d, bounces, rng);
 
   auto pt = intersect(scn, q, d);
   if(!pt.hit()) return pt.le;
@@ -517,9 +501,7 @@ vec3f estimate_li_naive(
   auto i = sample_brdfcos(pt, rng);
   auto li = estimate_li_naive(scn, pt.x,i,bounces-1,rng);
 
-  auto pdf = weight_brdfcos(pt,i);
-  auto eval = eval_brdfcos(pt,i);
-  auto lr = li * eval * pdf;
+  auto lr = li * eval_brdfcos(pt,i) * weight_brdfcos(pt,i);
 
   return pt.le + lr;
 
@@ -530,15 +512,40 @@ vec3f estimate_li_naive(
 vec3f estimate_li_product(
     const scene* scn, const vec3f& q, const vec3f& d, int bounces, rng_t& rng) {
   //TODO estimate_li_product
-  return {};
+
+  auto pt = intersect(scn, q, d);
+  auto li = pt.le;
+  vec3f w = {1,1,1};
+
+  for(auto bounce : range(bounces)) {
+    if(!pt.hit()) break;
+    auto i = sample_brdfcos(pt, rng);
+    auto bpt = intersect(scn, pt.x, i);
+    auto rrprob = 1.0f - ygl::min(ygl::max_element_val(pt.kd + pt.ks + pt.le), 0.95f);
+    //rrprob = 1/rrprob;
+    w *= eval_brdfcos(pt,i) * (rrprob*weight_brdfcos(pt,i)); // update w  //pr(pt.f)*p(i)
+    li += w * bpt.le;          // accumulate li
+    pt = bpt;                  // “recurse”
+  }
+  return li;
 }
 
 /// Pathtracing with direct+indirect and russian roulette
 vec3f estimate_li_direct(
     const scene* scn, const vec3f& q, const vec3f& d, int bounces, rng_t& rng) {
   //TODO estimate_li_direct
-  return {};
-}
+
+
+  auto pt = intersect(scn, q, d);
+  auto li = pt.le;
+  vec3f w = {1,1,1};
+  for(auto bounce : range(bounces)) {
+    if(!pt.hit()) break;
+    auto lpt = sample_lights(scn, pt, rng);
+    if(!intersect(scn, pt.x, -lpt.o).hit())
+      li += w*lpt.le * eval_brdfcos(pt,-lpt.o) * weight_lights(scn,lpt,pt);
+  }
+  return li;}
 
 /// Pathtracing with direct+indirect, MIS and russian roulette
 vec3f estimate_li_mis(
